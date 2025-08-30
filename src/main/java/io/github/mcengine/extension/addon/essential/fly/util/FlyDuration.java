9a13613853f0dbd9476afef1a52b715c6f375abf
JetsadaWijit <jetsadawijit@outlook.com>
2025-08-30 15:37:42 +0000

    fix: Correct msg position

--- src/main/java/io/github/mcengine/extension/addon/essential/fly/util/FlyDuration.java
+++ src/main/java/io/github/mcengine/extension/addon/essential/fly/util/FlyDuration.java
@@ -125,7 +125,7 @@ public class FlyDuration {
 
                     // Inform player of remaining time in formatted units
                     if (remaining > 0) {
-                        p.sendMessage("§cFlight disabled. §7Remaining: §e" + formatDuration(remaining) + "§7.");
+                        p.sendMessage("§7Remaining: §e" + formatDuration(remaining) + "§7.");
                     } else {
                         try {
                             p.setAllowFlight(false);
@@ -186,7 +186,7 @@ public class FlyDuration {
                     Player p = Bukkit.getPlayer(uuid);
                     if (p != null) {
                         if (remainingAfter > 0) {
-                            p.sendMessage("§7Remaining: §e" + formatDuration(remainingAfter) + "§7.");
+                            p.sendMessage("§cFlight disabled. §7Remaining: §e" + formatDuration(remainingAfter) + "§7.");
                         } else {
                             p.sendMessage("§cYour flight time has expired.");
                         }
